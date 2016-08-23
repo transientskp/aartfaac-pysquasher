f61cce0e05ee46478f47f3891aec1cf950f64df9
Error323 <folkerthuizinga@gmail.com>
2016-08-23 09:53:56 +0200

    add c++ header for reference

--- pysquasher.py
+++ pysquasher.py
@@ -8,8 +8,8 @@ import sys
 import os
 import datetime
 import argparse
-import gfft
 import logging
+import gfft
 
 LOG_FORMAT = "%(levelname)s %(asctime)s %(process)d %(filename)s:%(lineno)d] %(message)s"
 
@@ -46,6 +46,23 @@ def get_configuration():
 def parse_header(hdr):
     """
     Parse aartfaac header for calibrated data
+
+    struct output_header_t
+    {
+      uint64_t magic;                   ///< magic to determine header
+      double start_time;                ///< start time (unix)
+      double end_time;                  ///< end time (unix)
+      int32_t subband;                  ///< lofar subband
+      int32_t num_dipoles;              ///< number of dipoles (288 or 576)
+      int32_t polarization;             ///< XX=0, YY=1
+      int32_t num_channels;             ///< number of channels (<= 64)
+      float ateam_flux[5];              ///< Ateam fluxes (CasA, CygA, Tau, Vir, Sun)
+      std::bitset<5> ateam;             ///< Ateam active
+      std::bitset<64> flagged_channels; ///< bitset of flagged channels (8 byte)
+      std::bitset<576> flagged_dipoles; ///< bitset of flagged dipoles (72 byte)
+      uint32_t weights[78];             ///< stationweights n*(n+1)/2, n in {6, 12}
+      uint8_t pad[48];                  ///< 512 byte block
+    };
     """
     m, t0, t1, s, d, p, c = struct.unpack("<Qddiiii", hdr[0:40])
     assert(m == HDR_MAGIC)
