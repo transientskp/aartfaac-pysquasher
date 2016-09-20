67310a4d9839f196996bbd161d406bd680868e74
Mark <m.j.kuiack@uva.nl>
2016-09-20 15:39:01 +0200

    Add RMS back to header but without using "history"

--- pysquasher.py
+++ pysquasher.py
@@ -123,7 +123,7 @@ def write_fits(img, metadata, fitsobj):
     fitsobj.header['DATAMAX'] = high
     fitsobj.header['DATAMIN'] = low
     #fitsobj.header['HISTORY'] = 'AARTFAAC 6 stations superterp'
-    #fitsobj.header['HISTORY'] = 'RMS {}'.format(quality)
+    fitsobj.header['DATARMS'] = quality
     #fitsobj.header['HISTORY'] = 'DYNAMIC RANGE {}:{}'.format(int(round(high)), int(round(quality)))
     logger.info("%s %0.3f %i:%i", filename, quality, int(round(high)), int(round(quality)))
 
